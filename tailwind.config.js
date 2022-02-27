266cfb18986ea39a39798688cd02e296d1b01bd0
Nikruto <nikruto1456@gmail.com>
2022-02-27 13:09:38 +0300

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#333333",
+      },
+    },
+
+    fontFamily: {
+      mono: "Space Mono",
+      logo: "Inconsolata",
+    },
+  },
+  plugins: [],
+};
